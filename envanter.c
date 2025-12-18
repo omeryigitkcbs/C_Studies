11e46643358263c5fb27b1c6b29eeae00f4304dc
Omer Yigit Kocabas <omeryigitkocabas2@gmail.com>
2025-12-18 19:02:48 +0300

    First C Code

---
+++ envanter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 3 elemanlı bir dizi (Kılıç, Kalkan, İksir)
+    int envanter[3] = {1, 0, 5};
+
+    printf("Kilic: %d\n", envanter[0]);
+    printf("Kalkan: %d\n", envanter[1]);
+    printf("Iksir: %d\n", envanter[2]);
+
+    return 0;
+}
