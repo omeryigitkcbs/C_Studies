c54a274b498775dcfd9c7380236239dcaad6c02a
Omer Yigit Kocabas <omeryigitkocabas2@gmail.com>
2026-01-01 15:26:05 +0300

    Add scanf examples for user input.

---
+++ scanf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    // scanf ile kullanýcýdan veri alma örneði.
+    // En kritik nokta & iþaretidir. Girilen komutu yerine ulaþtýrmak için aracýlýk yapar.
+
+    int yas;
+    printf("Lutfen Yasinizi Giriniz:\n");
+    scanf("%d",&yas);
+    printf("Demek %d Yasindasin!\n");
+
+    return 0;
+}
